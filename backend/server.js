337dd23816b70793abf6f4f0c933d00b1d5f59eb
Sanketh Shetty <sankethshetty007@gmail.com>
2021-07-05 20:56:34 -0500

    puttign in some comments to make sense of it all

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
+
+// need it to create the main express app
 import express from "express"
+
 import cors from "cors"
+
+// 
 import restaurants from "./api/restaurants.route.js"
 
 const app = express()
@@ -7,10 +12,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
+// this is express routes
 app.use("/api/v1/restaurants", restaurants)
 
 app.use("*", (req, res) => res.status(404).json({error:"not found"}))
 
-
+// we will be seaprating the app.js and the server.js
 export default app
